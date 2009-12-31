569cdc068273bc7fd5fc0d915f9098b49eb92aeb
Matteo Pratesi <matteo.pratesi@libero.it>
2009-12-31 01:06:46 +0000

    pmango: tasknetwork: ora è possibile creare una task network completa (ossia unire piu immagini a destra o sotto tra di loro). viene fatto un esempio random. aggiungo anche la start e la end project milestone.
    git-svn-id: http://svn.xp-dev.com/svn/sw4us/pmango@305 ba209ffe-1281-4cf9-8354-64b18d68633c

--- tests/TaskNetwork/linee.php
+++ tests/TaskNetwork/linee.php
@@ -1,8 +1,7 @@
 <?
 Header("Content-Type: image/jpeg");
 
-include ("TaskNetwork.php");
-
+//------Funzioni di disegno------------
 function imageboldline($image, $x1, $y1, $x2, $y2, $color, $thick = 1)
 {
    /* this way it works well only for orthogonal lines
@@ -27,7 +26,6 @@ function imageboldline($image, $x1, $y1, $x2, $y2, $color, $thick = 1)
    imagefilledpolygon($image, $points, 4, $color);
    return imagepolygon($image, $points, 4, $color);
 }
-
 
 function arrow($im, $x1, $y1, $x2, $y2, $alength, $awidth, $color) {
     $distance = sqrt(pow($x1 - $x2, 2) + pow($y1 - $y2, 2));
@@ -71,62 +69,223 @@ function dashedarrow($im, $x1, $y1, $x2, $y2, $alength, $awidth, $color) {
     imagefilledpolygon($im, array($x2, $y2, $x3, $y3, $x4, $y4), 3, $color);
 }
 
-	
-	//esegue il merging di due immagini circondate da uno spazio di 25 px
-function mergeImgRight($a,$b){
+//------Funzioni di disegno------------fine	
 
-	$imga = $a["img"];
-	$imgax = $a["x"];
-	$imgay = $a["y"];
+//------Funzioni di merging------------
+// NOTA BENE di seguito con TN è indicata erroneamente una riga delle TN, le cambierò in seguito 
+//esegue il merging di due immagini separete da 50 px di spazio
+//da modificare in modo che unisca un array di immagini variabile
+function mergeImgRight($TN,$b){
+
+	$imgTN = $TN["img"];
+	$imgTNx = $TN["x"];
+	$imgTNy = $TN["y"];
 	
 	$imgb = $b["img"];
 	$imgbx = $b["x"];
 	$imgby = $b["y"];
 	
-	$outx = ($imgax+$imgbx+100);
-	$outy = $imgay/*$imgby*/+50;
+	//alloco i punti mediani dei lati della tbx dentro b
+	$b["leftx"] = 0;
+	$b["lefty"] = $imgby/2;  
+	$b["rightx"] = $imgbx;
+	$b["righty"] = $imgby/2;
+	
+	$outx = ($imgTNx+$imgbx+100);
+	$outy = max($imgTNy, $imgby);
 
 	
 	$out = ImageCreate($outx , $outy);
 	$bianco = ImageColorAllocate($out,255,255,255);
 	
 	//copio la prima immagine nell'output
-	imagecopy($out,$imga,25,25,0,0,$imgax,$imgay);
+	imagecopy($out,$imgTN,0,0,0,0,$imgTNx,$imgTNy);
 	//e poi la seconda
-	imagecopy($out,$imgb,$outx-($imgbx+25),25,0,0,$imgbx,$imgby);
+	imagecopy($out,$imgb,$outx-($imgbx+50),0,0,0,$imgbx,$imgby);
 	
-	$array["img"]=$out; $array["x"] = $outx; $array["y"] = $outy; 
-	return $array;
+	$index = $TN["index"]; $i = $TN["i"];$h = $TN["h"];
+	$index[$h][$i]= $b;
+	$i++; 
+	
+	
+	$TN["img"]=$out; $TN["x"] = $outx; $TN["y"] = $outy; $TN["index"] = $index; $TN["i"] = $i;
+	return $TN;
 }
 
-function mergeImgUnder($a,$b){
+//merging di due TN separate da 50 px
+function mergeTNUnder($TN,$TN2){
 
-	$imga = $a["img"];
-	$imgax = $a["x"];
-	$imgay = $a["y"];
+	$imgTN = $TN["img"];
+	$imgTNx = $TN["x"];
+	$imgTNy = $TN["y"];
+	$indexTN = $TN["index"];
 	
-	$imgb = $b["img"];
-	$imgbx = $b["x"];
-	$imgby = $b["y"];
+	$imgTN2 = $TN2["img"];
+	$imgTN2x = $TN2["x"];
+	$imgTN2y = $TN2["y"];
+	$indexTN2 = $TN2["index"][0];
 	
-	$outx = $imgax/*$imgby*/+50;
-	$outy = ($imgay+$imgby+100);
+		
+	$outx = max($imgTNx,$imgTN2x);
+	$outy = ($imgTNy+$imgTN2y);
 
 	
 	$out = ImageCreate($outx , $outy);
 	$bianco = ImageColorAllocate($out,255,255,255);
 	
-	//copio la prima immagine nell'output
-	imagecopy($out,$imga,25,25,0,0,$imgax,$imgay);
-	//e poi la seconda
-	imagecopy($out,$imgb,25,$outy-($imgby+25),0,0,$imgbx,$imgby);
+	//copio la prima immagine nell'output centrata
+	imagecopy($out,$imgTN,($outx/2)-($imgTNx/2),0,0,0,$imgTNx,$imgTNy);
+	//e poi la seconda centrata
+	imagecopy($out,$imgTN2,($outx/2)-($imgTN2x/2),$outy-$imgTN2y,0,0,$imgTN2x,$imgTN2y);
 	
-	$array["img"]=$out; $array["x"] = $outx; $array["y"] = $outy; 
-	return $array;
+	$h = $TN["h"];
+	$indexTN[$h]= $indexTN2;
+	$h++; 
+	
+	$TN["img"]=$out; $TN["x"] = $outx; $TN["y"] = $outy; $TN["index"] =$indexTN; $TN["h"]= $h;
+	return $TN;
 }
 
+function mergeArrayRight($TaskNetwork, $array){
 
-function connect($out,$im1,$im2,$where,$dash = false){
+	foreach($array as $tbx){
+		$TaskNetwork = mergeImgRight($TaskNetwork,$tbx);
+	}
+	
+		//ingrandisco il disegno di 25 px in alto e in basso
+		$out = ImageCreate($TaskNetwork["x"] , $TaskNetwork["y"]+50);
+		$bianco = ImageColorAllocate($out,255,255,255);
+		
+		imagecopy($out,$TaskNetwork["img"],0,25,0,0,$TaskNetwork["x"],$TaskNetwork["y"]);
+		
+		$TaskNetwork["img"]=$out; $TaskNetwork["y"] = $TaskNetwork["y"]+50;
+		
+				
+	return $TaskNetwork;
+	
+	
+}
+
+function mergeArrayUnder($TaskNetwork, $array){
+
+	foreach($array as $TNrow){
+
+		$TaskNetwork = mergeTNUnder($TaskNetwork,$TNrow);
+
+	}
+	
+	
+	//ora rendo assolute le coordinate relative delle tbx inserite
+		$TaskNetwork["index"] = transformCoordinateRow($TaskNetwork);
+
+	return $TaskNetwork;
+}
+//------Funzioni di merging------------fine
+function mergeSpM($TaskNetwork, $spm){
+	$imgTN = $TaskNetwork["img"];
+	$imgTNx = $TaskNetwork["x"];
+	$imgTNy = $TaskNetwork["y"];
+	
+	$imgspm = $spm["img"];
+	$imgspmx = $spm["x"];
+	$imgspmy = $spm["y"];
+	
+	
+	$outx = ($imgTNx+$imgspmx+200);
+	$outy = max($imgTNy, $imgspmy);
+
+	//alloco i punti mediani del lato destro della SpM
+	$spm["rightx"] = $imgspmx;
+	$spm["righty"] = $outy/2;
+	
+	$out = ImageCreate($outx , $outy);
+	$bianco = ImageColorAllocate($out,255,255,255);
+
+	//copio la prima SpM nell'output
+	imagecopy($out,$imgspm,0,($outy/2)-($imgspmy/2),0,0,$imgspmx,$imgspmy);
+	//e poi la TN
+	imagecopy($out,$imgTN,($outx-$imgTNx),0,0,0,$imgTNx,$imgTNy);
+	
+	$index = $TaskNetwork["index"]; $i = $TaskNetwork["i"];$h = $TaskNetwork["h"];
+	$index[$h][$i]= $spm;
+	$i++; 
+	
+	
+	$TaskNetwork["img"]=$out; $TaskNetwork["x"] = $outx; $TaskNetwork["y"] = $outy; $TaskNetwork["index"] = $index; $TaskNetwork["i"] = $i;
+	return $TaskNetwork;
+	
+}
+
+function mergeEpM($TaskNetwork, $epm){
+	$imgTN = $TaskNetwork["img"];
+	$imgTNx = $TaskNetwork["x"];
+	$imgTNy = $TaskNetwork["y"];
+	
+	$imgepm = $epm["img"];
+	$imgepmx = $epm["x"];
+	$imgepmy = $epm["y"];
+	
+	$outx = ($imgTNx+$imgepmx+200);
+	$outy = max($imgTNy, $imgepmy);
+
+	//alloco i punti mediani del lato destro della SpM
+	  
+	$epm["leftx"] = $outx-$imgepmx;
+	$epm["lefty"] = $outy/2;
+	
+	$out = ImageCreate($outx , $outy);
+	$bianco = ImageColorAllocate($out,255,255,255);
+
+	//copio la prima SpM nell'output
+	imagecopy($out,$imgTN,0,0,0,0,$imgTNx,$imgTNy);
+	//e poi la TN
+	imagecopy($out,$imgepm,($outx-$imgepmx),($outy/2)-($imgepmy/2),0,0,$imgepmx,$imgepmy);
+	
+	$index = $TaskNetwork["index"]; $i = $TaskNetwork["i"];$h = $TaskNetwork["h"];
+	$index[$h][$i]= $epm;
+	$i++; 
+	
+	
+	$TaskNetwork["img"]=$out; $TaskNetwork["x"] = $outx; $TaskNetwork["y"] = $outy; $TaskNetwork["index"] = $index; $TaskNetwork["i"] = $i;
+	return $TaskNetwork;
+	
+}
+//------Funzioni di setting------------
+function connect($TaskNetwork, $ID1,$ID2){
+	//ID 1 e 2 sono due array ID["riga"] ID["colonna"]
+	$im = $TaskNetwork;
+	$index = $TaskNetwork["index"];
+	$tbx1 = $index[$ID1["riga"]][$ID1["colonna"]];
+	$tbx2 = $index[$ID2["riga"]][$ID2["colonna"]];
+	
+	drawline($im,$tbx1,$tbx2,"right",true);
+	
+}
+function drawline($out,$im1,$im2,$where,$dash = false){
+	$black = ImageColorAllocate($out["img"],0,0,0);
+	switch($where){
+		case "right": if($dash==true) {dashedarrow ($out["img"], $im1["rightx"],$im1["righty"], $im2["leftx"],$im2["lefty"],5,5, $black);}
+					  else {arrow ($out["img"], $im1["rightx"],$im1["righty"], $im2["leftx"],$im2["lefty"],5,5, $black);}break;
+
+		//NON FUNZIONANTE//
+		case "under": if($dash==true) {
+						imagedashedline ($out["img"], $im1["x"]+25,(($im1["y"])/2)+25, $out["x"]-1,(($im1["y"])/2)+25, $black);
+						imagedashedline ($out["img"], $out["x"]-1,(($im1["y"])/2)+25, $out["x"]-1,$im1["y"]+50, $black);
+						imagedashedline ($out["img"], $out["x"],$im1["y"]+50, 0 ,$im1["y"]+50, $black);
+						imagedashedline ($out["img"],  0 ,$im1["y"]+50, 0 ,$out["y"]-((($im2["y"])/2)+25), $black);
+						dashedarrow ($out["img"], 0 ,$out["y"]-((($im2["y"])/2)+25), $out["x"]-($im2["x"]+25 ),$out["y"]-((($im2["y"])/2)+25),5,5, $black);
+					  }
+					  else {
+					  	imageline ($out["img"], $im1["x"]+25,(($im1["y"])/2)+25, $out["x"]-1,(($im1["y"])/2)+25, $black);
+						imageline ($out["img"], $out["x"]-1,(($im1["y"])/2)+25, $out["x"]-1,$im1["y"]+50, $black);
+						imageline ($out["img"], $out["x"],$im1["y"]+50, 0 ,$im1["y"]+50, $black);
+						imageline ($out["img"],  0 ,$im1["y"]+50, 0 ,$out["y"]-((($im2["y"])/2)+25), $black);
+						arrow ($out["img"], 0 ,$out["y"]-((($im2["y"])/2)+25), $out["x"]-($im2["x"]+25 ),$out["y"]-((($im2["y"])/2)+25),5,5, $black);
+					  }break;
+	}
+}
+//DEPRECATED
+/*function connect($out,$im1,$im2,$where,$dash = false){
 	$black = ImageColorAllocate($out["img"],0,0,0);
 	switch($where){
 		case "right": if($dash==true) {dashedarrow ($out["img"], $im1["x"]+25,(($im1["y"])/2)+25, $out["x"]-($im2["x"]+25),(($im2["y"])/2)+25,5,5, $black);}
@@ -146,35 +305,96 @@ function connect($out,$im1,$im2,$where,$dash = false){
 						arrow ($out["img"], 0 ,$out["y"]-((($im2["y"])/2)+25), $out["x"]-($im2["x"]+25 ),$out["y"]-((($im2["y"])/2)+25),5,5, $black);
 					  }break;
 	}
+}*/
+
+
+function transformCoordinateRow($TaskNetwork){
+	$x = 50; $y = 25;
+	$index = $TaskNetwork["index"];
+	
+	for($k=0;$k<count($index); $k++){
+		$tbx = $index[$k];
+		$tbx["leftx"] += $x;
+		$tbx["lefty"] += $y; 
+		$tbx["rightx"] += $x;
+		$tbx["righty"] += $y;
+		$x =  $tbx["rightx"]+100;
+		$index[$k] = $tbx;
+	}
+	
+	return $index;
+	
+}
+
+//------Funzioni di setting------------fine
+
+
+/// creazione TN vuota
+$index; $i=0;$h=0;
+$TaskNetwork["img"] = ImageCreate(1,1); //immagine vuota iniziale della TN
+$TaskNetwork["x"] = 1; 					//larghezza della TN
+$TaskNetwork["y"] = 1;					//altezza della TN
+$TaskNetwork["index"] = $index;			//matrice delle TBx presenti nella TN
+$TaskNetwork["i"] = $i;					//indice delle colonne precedenti
+$TaskNetwork["h"] = $h;					//indice delle righe precedenti
+
+//creazione startprojectmilestone
+$spm["img"] =  imageCreateFromPNG("startprojectmilestone.png");
+$size = getImageSize("startprojectmilestone.png");
+$spm["x"] =  $size[0];
+$spm["y"] =  $size[1];
+
+//creazione endprojectmilestone
+$epm["img"] =  imageCreateFromPNG("endprojectmilestone.png");
+$size = getImageSize("endprojectmilestone.png");
+$epm["x"] =  $size[0];
+$epm["y"] =  $size[1];
+
+
+//ogni immagine Image è dentro un array im dove im["img"] è l'immagine, im["x"] e im["y"] la x e la y
+$array;
+
+for($r=0;$r<5;$r++){
+	
+$l = rand ( 1 , 10 );
+
+	for ($j=0;$j<$l;$j++){
+		$im["img"] =  imageCreateFromPNG("TaskBoxTest.png");
+		$size = getImageSize("TaskBoxTest.png");
+		$im["x"] =  $size[0];
+		$im["y"] =  $size[1];
+		$array[$r][$j] = $im;
+	}
+$out[$r] = mergeArrayRight($TaskNetwork, $array[$r]);
+	
 }
 
 
-//ogni immagine Image è dentro un array image dove image["img"] è l'immagine, image["x"] e img["y"] la x e la y
+$final = mergeArrayUnder($TaskNetwork,$out);
+$finalSpm = mergeSpM($final, $spm);
+$finalpm = mergeEpM($finalSpm, $epm);
 
-$im1["img"] =  imageCreateFromPNG("TaskBoxTest.png");
-$size1 = getImageSize("TaskBoxTest.png");
-$im1["x"] =  $size1[0];
-$im1["y"] =  $size1[1];
 
-$im2["img"] =  imageCreateFromPNG("TaskBoxTest.png");
-$size2 = getImageSize("TaskBoxTest.png");
-$im2["x"] =  $size2[0];
-$im2["y"] =  $size2[1];
+$ID1["riga"] = $finalpm["h"]; $ID1["colonna"] = 0;
+$ID2["riga"] = $finalpm["h"]; $ID2["colonna"] = 1;  
+connect($finalpm,$ID1,$ID2);
 
+ 
+/* DEPRECATED
 $out = mergeImgUnder($im1,$im2);
 $out2 = mergeImgRight($im1,$im2);
 
 //connette le due immagini una sotto l'altra con linea tratteggiata
-//connect($out,$im1,$im2,"under",true);
+//drawline($out,$im1,$im2,"under",true);
 //connette le due immagini una sotto l'altra con linea unita
-//connect($out,$im1,$im2,"under");
+//drawline($out,$im1,$im2,"under");
 //connette le due immagini una accanto l'altra con linea tratteggiata
-//connect($out2,$im1,$im2,"right",true);
+//drawline($out2,$im1,$im2,"right",true);
 //connette le due immagini una accanto l'altra con linea unita
-connect($out2,$im1,$im2,"right");
+//drawline($out2,$im1,$im2,"right");
 
+*/
 
-
-Imagepng($out2["img"]);
-ImageDestroy($out2["img"]);
+Imagepng($finalpm["img"]);
+ImageDestroy($finalpm["img"]);
 ?>
