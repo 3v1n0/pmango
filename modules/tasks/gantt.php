55701446ea73283fa010105dfd7400084a64480c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2010-01-13 09:35:50 +0000

    pmango: GANTT, use a dark gray background in a parent actual progress bar
    git-svn-id: http://svn.xp-dev.com/svn/sw4us/pmango@383 ba209ffe-1281-4cf9-8354-64b18d68633c

--- modules/tasks/gantt.php
+++ modules/tasks/gantt.php
@@ -796,7 +796,7 @@ for($i = 0; $i < count(@$gantt_arr); $i ++ ) {
 			} else {
 				$bar2->SetColor('black');
 				$bar2->SetFillColor('black');
-				$bar2->SetPattern(BAND_SOLID,'black');
+				$bar2->SetPattern(BAND_SOLID,'gray3');
 
 				$bar2->progress->SetFillColor($colors ? 'green' : 'gray6');
 				$bar2->progress->SetPattern(BAND_SOLID, $colors ? 'green' : 'gray6', 98);
@@ -818,8 +818,8 @@ for($i = 0; $i < count(@$gantt_arr); $i ++ ) {
 				$bar2->rightMark->SetWidth(2);
 
 				if ($progress != 100) {
-					$bar2->rightMark->SetColor('black');
-					$bar2->rightMark->SetFillColor('black');
+					$bar2->rightMark->SetColor('gray3');
+					$bar2->rightMark->SetFillColor('gray3');
 				} else {
 					$bar2->rightMark->SetColor($colors ? 'green' : 'gray6');
 					$bar2->rightMark->SetFillColor($colors ? 'green' : 'gray6');
