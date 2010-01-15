996d068018f2de791323c3d03d390d11f0e5b31a
Marco Trevisan (Treviño) <mail@3v1n0.net>
2010-01-15 08:19:56 +0000

    pmango: GANTT view, correctly reset the opened/closed tasks on level change.
    git-svn-id: http://svn.xp-dev.com/svn/sw4us/pmango@433 ba209ffe-1281-4cf9-8354-64b18d68633c

--- modules/tasks/viewgantt.php
+++ modules/tasks/viewgantt.php
@@ -64,6 +64,8 @@ $project_id = defVal( @$_GET['project_id'], 0);
 if (!empty($_POST)) {
 	$task_level = dPgetParam($_POST, 'explode_tasks', '1');
 	$AppUI->setState('ExplodeTasks', $task_level);
+	$AppUI->setState('tasks_opened', array());
+	$AppUI->setState('tasks_closed', array());
 }
 
 $sql = "SELECT project_start_date, project_finish_date FROM projects WHERE project_id = $project_id";
