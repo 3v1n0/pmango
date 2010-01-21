75747690e07e2b670f6abb45d67714b6d0fac39a
Andrea Toccafondi <tocca.88@hotmail.it>
2010-01-21 15:14:30 +0000

    pmango: prove di TaskBox.php con aggiunta con ID del task, del suo nome, eventuale errore (grave/non grave), risorse pianificate del task (durata, effort e budget), data inizio e fine del task (pianificata), nome di tutti i partecipanti con il loro relativo effort, data inizio e data fine attuali, risorse attuali del task (durata, effort e budget), e barra di progresso.
    git-svn-id: http://svn.xp-dev.com/svn/sw4us/pmango@527 ba209ffe-1281-4cf9-8354-64b18d68633c

--- tests/planned/TaskBox.php
+++ tests/planned/TaskBox.php
@@ -33,6 +33,41 @@ outTest($tbx, "1.1.1.3");
 $tbx->setPlannedData("20 d", "60 ph", "2000 €");
 outTest($tbx, "1.1.1.4");
 
+$tbx->setPlannedTimeframe("21/12/2009","11/2/2010");
+$tbx->addResources("Andrea Toccafondi","Test enginee", "23");
+$tbx->addResources("Francesco Purpura","Test enginee", "45");
+$tbx->addResources("Marco Trevisa","Test enginee", "60");
+$tbx->addResources("Matteo Pratesi","Test enginee", "20");
+outTest($tbx, "1.1.1.5");
+
+$tbx->setResourcesArray(null);
+$tbx->setPlannedTimeframe(null,null);
+$tbx->setAlerts(TaskBox::ALERT_ERROR);
+$tbx->setActualTimeframe("10/08/2009","11/11/2009");
+$tbx->setProgress(50);
+outTest($tbx, "1.1.1.6.1");
+
+$tbx->setAlerts(null);
+$tbx->setAlerts(TaskBox::ALERT_WARNING);
+outTest($tbx, "1.1.1.6.2");
+
+$tbx->setAlerts(null);
+$tbx->setAlerts(TaskBox::ALERT_ERROR);
+$tbx->setPlannedTimeframe("21/12/2009","11/2/2010");
+$tbx->setActualTimeframe("10/08/2009","11/11/2009");
+$tbx->addResources("Andrea Toccafondi","Test enginee", "23");
+$tbx->addResources("Francesco Purpura","Test enginee", "45");
+$tbx->addResources("Marco Trevisa","Test enginee", "60");
+$tbx->addResources("Matteo Pratesi","Test enginee", "20");
+$tbx->addResources("Andrea Toccafondi","Test enginee", "23", "30");
+$tbx->addResources("Francesco Purpura","Test enginee", "23", "30");
+$tbx->addResources("Marco TRevisan","Test enginee", "23", "30");
+$tbx->addResources("Matteo Pratesi","Test enginee", "23", "30");
+outTest($tbx, "1.1.1.7.1");
+
+$tbx->setAlerts(null);
+$tbx->setAlerts(TaskBox::ALERT_WARNING);
+outTest($tbx, "1.1.1.7.2");
 ?>
 
 </body>
