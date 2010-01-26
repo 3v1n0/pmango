0e43003fc8b6153ad829c8e39bd4aff7a5ec6658
Marco Trevisan (Treviño) <mail@3v1n0.net>
2010-01-26 07:49:25 +0000

    pmango, Web UI, support for jQuery for loading graphs
    git-svn-id: http://svn.xp-dev.com/svn/sw4us/pmango@567 ba209ffe-1281-4cf9-8354-64b18d68633c

--- style/default/header.php
+++ style/default/header.php
@@ -68,9 +68,11 @@ else
 	<meta name="Version" content="<?php echo @$AppUI->getVersion();?>" />
 	<meta http-equiv="Content-Type" content="text/html;charset=<?php echo isset( $locale_char_set ) ? $locale_char_set : 'UTF-8';?>" />
 	<title><?php echo @dPgetConfig( 'page_title' );?></title>
-	<link rel="stylesheet" type="text/css" href="./style/<?php echo $uistyle;?>/main.css" media="all" />
-	<style type="text/css" media="all">@import "./style/<?php echo $uistyle;?>/main.css";</style>
-	<link rel="shortcut icon" href="./style/<?php echo $uistyle;?>/images/favicon.jpg" type="image/ico" />
+	<link type="text/css" href="./style/<?php echo $uistyle;?>/main.css" rel="stylesheet" media="all" />
+	<link type="text/css" href="./style/<?php echo $uistyle;?>/jquery.ui.resizable.css" rel="stylesheet" />
+	<link type="text/css" href="./style/<?php echo $uistyle;?>/jquery.ui.theme.css" rel="stylesheet" />
+	<link type="text/css" href="./style/<?php echo $uistyle;?>/jquery.iviewer.css" rel="stylesheet" />
+	<link type="image/ico" href="./style/<?php echo $uistyle;?>/images/favicon.jpg" rel="shortcut icon" />
 	<?php @$AppUI->loadJS(); ?>
 </head>
 <!--Serve per far rientrare il contenuto dello schermo, lasciare un po' di spazi ai bordi-->
