271a2f13e81894ef033c7599250162055832a906
Marco Trevisan (Treviño) <mail@3v1n0.net>
2010-01-26 04:34:54 +0000

    pmango: ImgBlock, added the LGPL v3 license statement.
    git-svn-id: http://svn.xp-dev.com/svn/sw4us/pmango@556 ba209ffe-1281-4cf9-8354-64b18d68633c

--- tests/ImgBlock.class.php
+++ tests/ImgBlock.class.php
@@ -1,4 +1,22 @@
 <?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * ImageBlock, a small php library for building simple nested gd based widgets *
+ *                                                                             *
+ * Copyright (C) 2009-2010 Marco Trevisan (Treviño) <mail@3v1n0.net>           *
+ *                                                                             *
+ * This program is free software: you can redistribute it and/or modify        *
+ * it under the terms of the GNU Lesser General Public License as published by *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU Lesser General Public License    *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.       *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 abstract class ImgBlock {
 	private $pFgColor;
