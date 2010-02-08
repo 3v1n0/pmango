8e518ea0a85b7d7e585f5be28475e1fd7aa88e88
Marco Trevisan (Treviño) <mail@3v1n0.net>
2010-02-08 06:34:35 +0000

    pmango: projectState, define project key
    git-svn-id: http://svn.xp-dev.com/svn/sw4us/pmango@701 ba209ffe-1281-4cf9-8354-64b18d68633c

--- functions/project_state.php
+++ functions/project_state.php
@@ -1,7 +1,15 @@
 <?php
 
+define("PROJECTS_STATE_KEY", 'ProjectsState');
+
 if (!isset($project_id))
 	$project_id = $_REQUEST['project_id'];
+	
+function unsetProjectsStates() {
+	global $AppUI;
+	
+	$AppUI->unsetState(PROJECTS_STATE_KEY);
+}
 
 function setProjectState($label, $value = null) {
 	global $AppUI, $project_id;
@@ -9,25 +17,25 @@ function setProjectState($label, $value = null) {
 	if (is_null($value)) return;
 
 	if ($label === null)
-		$AppUI->state['ProjectsState'][$project_id] = $value;
+		$AppUI->state[PROJECTS_STATE_KEY][$project_id] = $value;
 	else
-		$AppUI->state['ProjectsState'][$project_id][$label] = $value;
+		$AppUI->state[PROJECTS_STATE_KEY][$project_id][$label] = $value;
 }
 
 function unsetProjectState($parent, $label) {
 	global $AppUI, $project_id;
 	
 	if($label !== null)
-		unset($AppUI->state['ProjectsState'][$project_id][$label]);
+		unset($AppUI->state[PROJECTS_STATE_KEY][$project_id][$label]);
 }
 
 function getProjectState($label, $default_value = null ) {	
 	global $AppUI, $project_id;
 	
 	if ($label === null)
-		$state = $AppUI->state['ProjectsState'][$project_id];
+		$state = $AppUI->state[PROJECTS_STATE_KEY][$project_id];
 	else
-		$state = $AppUI->state['ProjectsState'][$project_id][$label];
+		$state = $AppUI->state[PROJECTS_STATE_KEY][$project_id][$label];
 	
 	if (!is_null($state)) {
 		return $state;
@@ -44,20 +52,20 @@ function setProjectSubState($parent, $label, $value = null) {
 	
 	if (is_null($value)) return;
 	
-	$AppUI->state['ProjectsState'][$project_id][$parent][$label] = $value;
+	$AppUI->state[PROJECTS_STATE_KEY][$project_id][$parent][$label] = $value;
 }
 
 function unsetProjectSubState($parent, $label) {
 	global $AppUI, $project_id;
 	
 	if($label !== null)
-		unset($AppUI->state['ProjectsState'][$project_id][$parent][$label]);
+		unset($AppUI->state[PROJECTS_STATE_KEY][$project_id][$parent][$label]);
 }
 
-function getProjectSubState($parent, $label, $default_value = null ) {	
+function getProjectSubState($parent, $label, $default_value = null) {	
 	global $AppUI, $project_id;
 	
-	$state = $AppUI->state['ProjectsState'][$project_id][$parent][$label];
+	$state = $AppUI->state[PROJECTS_STATE_KEY][$project_id][$parent][$label];
 	
 	if (!is_null($state)) {
 		return $state;
